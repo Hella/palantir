8d9532237239626f602a0eb160e6e21dfa1bcadc
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2021-12-10 01:28:31 +0000

    Configure base simulation example

--- palantir/main.py
+++ palantir/main.py
@@ -1,3 +1,6 @@
+from random import gauss, uniform
+from typing import List
+
 from argparse import ArgumentParser
 from sqlalchemy.orm import sessionmaker
 
@@ -6,7 +9,16 @@ from palantir.crawlers.coingecko import (
     Interval,
     market_chart,
 )
+from palantir.clock import Clock
+from palantir.constants import GAUSS_RANDOM_SLIPPAGE, NULL_FEES
 from palantir.db import init_db, Quote
+from palantir.ithil import Ithil
+from palantir.liquidator import Liquidator
+from palantir.metrics import MetricsLogger
+from palantir.oracle import PriceOracle
+from palantir.simulation import Simulation
+from palantir.trader import Trader
+from palantir.types import Account, Currency
 
 
 VS_CURRENCY = "usd"
@@ -48,5 +60,60 @@ def crawl():
 
 
 def backtest():
-    # TODO backtest entry point
-    pass
+    db = init_db()
+
+    Session = sessionmaker(bind=db)
+    session = Session()
+
+    # XXX number of time samples to run the simulation on
+    samples = 30
+    read_quotes_from_db = lambda token: list(
+        session
+        .query(Quote)
+        .filter(Quote.coin==token)
+        .order_by(Quote.timestamp)
+        .all()
+    )[-samples:]
+
+    clock = Clock()
+    metrics_logger = MetricsLogger(clock)
+    ithil=Ithil(
+        apply_fees=NULL_FEES,
+        apply_slippage=GAUSS_RANDOM_SLIPPAGE,
+        clock=clock,
+        metrics_logger=metrics_logger,
+        price_oracle=PriceOracle(
+            clock=clock,
+            quotes={
+                Currency("dai"): read_quotes_from_db("dai"),
+                Currency("ethereum"): read_quotes_from_db("ethereum")
+            },
+        ),
+        vaults={
+            Currency("dai"): 750000.0,
+            Currency("ethereum"): 300.0,
+        },
+    )
+    simulation = Simulation(
+        clock=Clock(),
+        ithil=ithil,
+        liquidators=[
+            Liquidator(
+                ithil=ithil,
+                liquidation_probability=1.00, # We have a sniper liquidator here!
+            ),
+        ],
+        traders=[
+            # XXX we have a lonely trader here
+            Trader(
+                account=Account("aaaaa"),
+                open_position_probability=0.1,
+                close_position_probability=0.3,
+                ithil=ithil,
+                calculate_collateral=lambda: abs(gauss(mu=3000, sigma=5000)) + 100.0,
+                calculate_leverage=lambda: uniform(1.0, 10.0),
+            ),
+        ],
+    )
+
+    simulation.run()
