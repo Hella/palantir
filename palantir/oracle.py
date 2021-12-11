0a28f96e133107861e4dd23808519574cc3c8c81
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2021-12-11 20:22:32 +0000

    Delete reduntant field in PriceOracle

--- palantir/oracle.py
+++ palantir/oracle.py
@@ -7,7 +7,6 @@ from palantir.types import Currency, Price
 
 class PriceOracle:
     clock: Clock
-    max_period: int = 0
     quotes: Dict[Currency, List[Quote]] = {}
 
     def __init__(self, clock: Clock, quotes: Dict[Currency, List[Quote]]) -> None:
@@ -16,12 +15,9 @@ class PriceOracle:
         assert len(set(quote_periods)) == 1, "All price quote series must have the same length"
 
         self.clock = clock
-        self.max_period = quote_periods[0]
         self.quotes = quotes
 
     def get_price(self, src_token: Currency, dst_token: Currency) -> Price:
-        assert self.clock.time < self.max_period, "Simulation has ended"
-
         src_token_price = self.quotes[src_token][self.clock.time].price
         dst_token_price = self.quotes[dst_token][self.clock.time].price
 
